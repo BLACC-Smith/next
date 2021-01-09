8ad18eca12338cf3b4f4e99fb1807e43145c54a0
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-01-09 15:44:09 -0600

    minor update

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,6 @@ class MyDocument extends Document {
 						sizes="16x16"
 						href="/favicons/favicon-16x16.png"
 					/>
-					<link rel="manifest" href="/site.webmanifest" />
 					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
 					<meta name="msapplication-TileColor" content="#000000" />
 					<meta
