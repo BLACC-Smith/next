f180de3e2a802a69c1b5e63665f32f81bed2077e
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-02-16 11:25:26 -0600

    added cta tooltip

--- components/molecules/UpcomingEvent.js
+++ components/molecules/UpcomingEvent.js
@@ -64,6 +64,7 @@ const InlineCTA = styled.a`
 	font-weight: 600;
 	margin-left: 12px;
 	box-shadow: 2px 12px 24px rgba(145, 71, 255, 0.5);
+	cursor: not-allowed;
 
 	@media (max-width: 600px) {
 		display: none;
@@ -81,6 +82,46 @@ const CTA = styled.a`
 	background: #9147ff;
 	font-weight: 600;
 	box-shadow: 2px 12px 24px rgba(145, 71, 255, 0.5);
+	cursor: not-allowed;
+
+	@media (max-width: 600px) {
+		margin-top: 16px;
+		display: inline-flex;
+	}
+`;
+
+const InlineSpan = styled.span`
+	padding: ${({ inline }) => (inline ? '8px 16px' : ' 12px 24px')};
+	border-radius: 4px;
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	color: #fff;
+	text-align: center;
+	font-size: 16px;
+	background: #9147ff;
+	font-weight: 600;
+	margin-left: 12px;
+	box-shadow: 2px 12px 24px rgba(145, 71, 255, 0.5);
+	cursor: not-allowed;
+
+	@media (max-width: 600px) {
+		display: none;
+	}
+`;
+const Span = styled.span`
+	padding: ${({ inline }) => (inline ? '8px 16px' : ' 12px 24px')};
+	border-radius: 4px;
+	display: none;
+	justify-content: center;
+	align-items: center;
+	color: #fff;
+	text-align: center;
+	font-size: ${({ inline }) => (inline ? '16px' : '24px')};
+	background: #9147ff;
+	font-weight: 600;
+	box-shadow: 2px 12px 24px rgba(145, 71, 255, 0.5);
+	cursor: not-allowed;
 
 	@media (max-width: 600px) {
 		margin-top: 16px;
@@ -94,18 +135,22 @@ export default function UpcomingEvent() {
 			<LeftContainer>
 				<div style={{ display: 'flex' }}>
 					<Title>Upcoming Event</Title>
-					<InlineCTA
+					<InlineSpan inline data-tooltip="This event hasn't started yet">
+						JOIN EVENT
+					</InlineSpan>
+					{/* <InlineCTA
 						inline
 						href="https://www.twitch.tv/blacc_xyz"
 						target="_blank"
 					>
 						JOIN EVENT
-					</InlineCTA>
+					</InlineCTA> */}
 				</div>
 				<Undraw src="/undraw_events.png" layout="fill" objectFit="contain" />
-				<CTA href="https://www.twitch.tv/blacc_xyz" target="_blank">
+				{/* <CTA href="https://www.twitch.tv/blacc_xyz" target="_blank">
 					JOIN EVENT
-				</CTA>
+				</CTA> */}
+				<Span data-tooltip="This event hasn't started yet">JOIN EVENT</Span>
 			</LeftContainer>
 			<RightContainer>
 				<BoringImg src="/event.png" layout="fill" objectFit="contain" />
