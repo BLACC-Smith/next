3078db91706058323488d759cec04036debb37ab
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-02-19 16:23:49 -0600

    added page wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import styled from '@emotion/styled';
+import Header from '../components/atoms/Header';
+
+const Wrapper = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background: #000;
+	position: relative;
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Wrapper>
+			<Header />
+			<Component {...pageProps} />
+		</Wrapper>
+	);
 }
 
-export default MyApp
+export default MyApp;
