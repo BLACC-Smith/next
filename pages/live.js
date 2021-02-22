56e4846fc080b4758e8ba70b30aff16abf890e63
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-02-22 16:24:00 -0600

    updated grid

--- pages/live.js
+++ pages/live.js
@@ -9,7 +9,7 @@ const Container = styled.div`
 	height: 100vh;
 	background: #000;
 `;
-const Wrapper = styled.div`
+const Grid = styled.div`
 	height: 100%;
 	width: 100%;
 	display: grid;
@@ -18,6 +18,23 @@ const Wrapper = styled.div`
 
 	@media (max-width: 600px) {
 		grid-template-columns: 1fr;
+	}
+`;
+const Stream = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	margin-top: 117px;
+	// background: yellow;
+	justify-self: flex-end;
+
+	@media (max-width: 600px) {
+		height: calc(100% - 117px);
+		grid-template-columns: 1fr;
+		justify-content: center;
+		align-items: flex-end;
 	}
 `;
 const IframeContainer = styled.div`
@@ -30,7 +47,15 @@ const IframeContainer = styled.div`
 	border: 1px solid #757575;
 
 	@media (max-width: 600px) {
+		height: 85%;
 		width: 85%;
+	}
+`;
+const CTAContainer = styled.div`
+	margin-top: 117px;
+
+	@media (max-width: 600px) {
+		margin-top: 0;
 	}
 `;
 const CTA = styled.a`
@@ -49,9 +74,6 @@ const CTA = styled.a`
 	cursor: pointer;
 `;
 
-const CTAContainer = styled.div`
-	margin-top: 32px;
-`;
 const Label = styled.p`
 	font-size: 18px;
 	color: #fff;
@@ -105,39 +127,43 @@ export default function Live() {
 				{!width || !height ? (
 					<p>Loading</p>
 				) : (
-					<Wrapper>
-						<IframeContainer>
-							<iframe
-								src="https://embed.restream.io/player/index.html?token=e2022a75ef752141417cf11887150a39"
-								width={width}
-								height={height}
-								frameBorder="0"
-								allowFullScreen
-							/>
-						</IframeContainer>
-						<CTAContainer>
-							<CTA
-								bg="#9147ff"
-								shadow="rgba(145, 71, 255, 0.5)"
-								href="https://www.twitch.tv/blacc_xyz"
-								target="_blank"
-							>
-								<ImTwitch />
-								<Label>Watch Live on Twitch</Label>
-							</CTA>
-							<CTA
-								bg="#ff0202"
-								shadow="rgba(255, 2, 2, 0.5)"
-								href="https://www.youtube.com/channel/UCaTP89xCILPWN9qfA9Ike2A"
-								target="_blank"
-							>
-								<ImYoutube />
-								<Label>Watch Live on Youtube</Label>
-							</CTA>
-						</CTAContainer>
-					</Wrapper>
+					<>
+						<Grid>
+							<Stream>
+								<IframeContainer>
+									<iframe
+										src="https://embed.restream.io/player/index.html?token=e2022a75ef752141417cf11887150a39"
+										width={width}
+										height={height}
+										frameBorder="0"
+										allowFullScreen
+									/>
+								</IframeContainer>
+							</Stream>
+							<CTAContainer>
+								<CTA
+									bg="#9147ff"
+									shadow="rgba(145, 71, 255, 0.5)"
+									href="https://www.twitch.tv/blacc_xyz"
+									target="_blank"
+								>
+									<ImTwitch />
+									<Label>Watch Live on Twitch</Label>
+								</CTA>
+								<CTA
+									bg="#ff0202"
+									shadow="rgba(255, 2, 2, 0.5)"
+									href="https://www.youtube.com/channel/UCaTP89xCILPWN9qfA9Ike2A"
+									target="_blank"
+								>
+									<ImYoutube />
+									<Label>Watch Live on Youtube</Label>
+								</CTA>
+							</CTAContainer>
+						</Grid>
+						<UpcomingEvent />
+					</>
 				)}
-				<UpcomingEvent />
 			</Container>
 		</>
 	);
