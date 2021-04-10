8e6650252fbb53a19301f4365579decad8841cce
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2021-04-10 14:06:43 -0400

    Added link for podcast

--- components/molecules/LiveEvents.js
+++ components/molecules/LiveEvents.js
@@ -22,9 +22,10 @@ export default function LiveEvents() {
 		<Container>
 			<Title>Upcoming Events</Title>
 			<Event
-				img="/4_10_21.png"
-				title="🎙 Whiteness in Digital Products"
-				date="Apr 10, 2021"
+				img='/4_10_21.png'
+				title='🎙 Whiteness in Digital Products'
+				date='Apr 10, 2021'
+				url='https://www.youtube.com/watch?v=PSXEQ4X5T20&ab_channel=BLACCSmith'
 			/>
 		</Container>
 	);
