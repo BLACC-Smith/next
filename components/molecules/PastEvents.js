5afc272401aba826018b586b14bec3226f5ba5db
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-04-20 20:17:16 -0500

    added new live event (#18)

--- components/molecules/PastEvents.js
+++ components/molecules/PastEvents.js
@@ -16,17 +16,29 @@ const Title = styled.p`
 	color: #fff;
 	margin-bottom: 48px;
 `;
+const Wrapper = styled.div`
+	display: grid;
+	row-gap: 24px;
+`;
 
 export default function PastEvents() {
 	return (
 		<Container>
 			<Title>Past Events</Title>
-			<Event
-				img="/2_21_21.jpg"
-				title="Live Coding Session: React + Firebase"
-				date="Feb 21, 2021"
-				url="https://www.youtube.com/watch?v=IJxaRsUtY-8"
-			/>
+			<Wrapper>
+				<Event
+					img="/4_10_21.png"
+					title="🎙 Whiteness in Digital Products"
+					date="Apr 10, 2021"
+					url="https://www.youtube.com/watch?v=PSXEQ4X5T20&ab_channel=BLACCSmith"
+				/>
+				<Event
+					img="/2_21_21.jpg"
+					title="Live Coding Session: React + Firebase"
+					date="Feb 21, 2021"
+					url="https://www.youtube.com/watch?v=IJxaRsUtY-8"
+				/>
+			</Wrapper>
 		</Container>
 	);
 }
