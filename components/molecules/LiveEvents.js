f759f27e5603886874d6450fd4828d746d2273de
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2021-05-07 21:02:52 -0400

    Adding wrapper to LiveEvents files

--- components/molecules/LiveEvents.js
+++ components/molecules/LiveEvents.js
@@ -17,17 +17,24 @@ const Title = styled.p`
 	margin-bottom: 48px;
 `;
 
+const Wrapper = styled.div`
+	display: grid;
+	row-gap: 24px;
+`;
+
 export default function LiveEvents() {
 	return (
 		<Container>
 			<Title>Upcoming Events</Title>
-			<Event
-				img='/twitch.PNG'
-				title='Building with the Twitch API and Github Actions'
-				date='May 8th, 2021'
-				url='https://www.twitch.tv/blacc_xyz'
-				live
-			/>
+			<Wrapper>
+				<Event
+					img='/twitch.PNG'
+					title='Building with the Twitch API and Github Actions'
+					date='May 8th, 2021'
+					url='https://www.twitch.tv/blacc_xyz'
+					live
+				/>
+			</Wrapper>
 		</Container>
 	);
 }
